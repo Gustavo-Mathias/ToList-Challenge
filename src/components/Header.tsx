ce25ca1f7a16ccd91054f4d749a3c2f9a1c3cb41
Gustavo-Mathias <gumathias1304@gmail.com>
2025-03-31 11:18:17 -0300

    alterações de caminhos dos icones

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import styles from "./Header.module.css";
 export function Header() {
     return (
         <header className={styles.container}>
-            <img src="./src/assets/logo.svg" alt="Logo da aplicação" />
+            <img src="public/logo.svg" alt="Logo da aplicação" />
         </header>
     )
 }
