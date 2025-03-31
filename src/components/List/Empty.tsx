c3e58c5ecb173e4bf69e4e462633de804f059d0a
Gustavo-Mathias <gumathias1304@gmail.com>
2025-03-31 11:26:34 -0300

    updates de caminho

--- src/components/List/Empty.tsx
+++ src/components/List/Empty.tsx
@@ -3,7 +3,7 @@ import styles from './Empty.module.css'
 export function Empty() {
   return (
     <div className={styles.container}>
-      <img src="public/clipboard.svg" alt="ícone de prancheta" />
+      <img src={`${import.meta.env.BASE_URL}clipboard.svg`} alt="Ícone de prancheta" />
       <p>
         <strong>Você ainda não tem tarefas cadastradas</strong>
         Crie tarefas e organize seus itens a fazer
