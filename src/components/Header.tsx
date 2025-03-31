859d18ed4dd3fbb21edd59e48004915d2bb4cbee
Gustavo-Mathias <gumathias1304@gmail.com>
2025-03-31 11:42:21 -0300

    updates de nomenclaturas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import styles from "./Header.module.css";
 export function Header() {
     return (
         <header className={styles.container}>
-            <img src={`${import.meta.env.BASE_URL}logo.svg`} alt="Logo da aplicação" />
+            <img src={`${import.meta.env.BASE_URL}Logo.svg`} alt="Logo da aplicação" />
         </header>
     )
 }
